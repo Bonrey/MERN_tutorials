9a63eb09c85e01805bb987522b6fa8871bd4fa64
bonrey <m.vologodskiy@gmail.com>
2021-06-13 21:12:46 +0300

    Set up dart-sass

--- react-router_dev-ed/src/components/App.js
+++ react-router_dev-ed/src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import '../App.scss';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+
 
 const App = () => {
   return (
-    <h1>Application</h1>
+    <div>
+      <h1>Application</h1>
+      <Nav />
+      <About />
+      <Shop />
+    </div>
   );
 }
 export default App;
